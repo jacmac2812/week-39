93de3ba6b9781451e864764f0fbdfd051efed252
Jacob Simonsen <jacobsimonsen@Jacobs-iMac.local>
2020-09-26 11:52:07 +0200

    part 1, 2, 3 added week 39

---
+++ part 1,2,3 week 39/src/main/java/dtomappers/PersonsDTO.java
@@ -0,0 +1,21 @@
+package dtomappers;
+
+import entities.Person;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PersonsDTO {
+    private List<PersonDTO> all = new ArrayList();
+
+    public PersonsDTO(List<Person> personEntities) {
+        personEntities.forEach((p) -> {
+            all.add(new PersonDTO(p));
+        });
+    }
+
+    public List<PersonDTO> getAll() {
+        return all;
+    }
+    
+}
